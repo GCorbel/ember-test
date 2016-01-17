a2e19eb1ede89c53d5e7f44516acc39a1ae3ba9d
GCorbel <guirec.corbel@gmail.com>
2016-01-17 07:47:14 -0500

    Setup production server

--- frontend/config/environment.js
+++ frontend/config/environment.js
@@ -22,16 +22,24 @@ module.exports = function(environment) {
       'default-src': "'self' https://checkout.stripe.com",
       'script-src': "'self' https://cdn.mxpnl.com https://checkout.stripe.com", // Allow scripts from https://cdn.mxpnl.com
       'font-src': "'self' http://fonts.gstatic.com", // Allow fonts to be loaded from http://fonts.gstatic.com
-      'connect-src': "'self' http://localhost:3001 http://localhost:5000 http://107.170.3.185:5000", // Allow data (ajax/websocket) from api.mixpanel.com and custom-api.local
+      'connect-src': "'self' http://localhost:3001 http://localhost:5000 http://192.203.9.104:5000", // Allow data (ajax/websocket) from api.mixpanel.com and custom-api.local
       'img-src': "'self' https://q.stripe.com",
       'style-src': "'self' 'unsafe-inline' https://fonts.googleapis.com", // Allow inline styles and loaded CSS from http://fonts.googleapis.com
       'media-src': "'self'"
     }
   };
 
-  ENV.APP.API_HOST = (process.env.API_HOST || 'http://localhost:5000')
   if (environment === 'development') {
     ENV.APP.API_HOST = (process.env.API_HOST || 'http://localhost:5000')
+    // ENV.APP.LOG_RESOLVER = true;
+    // ENV.APP.LOG_ACTIVE_GENERATION = true;
+    // ENV.APP.LOG_TRANSITIONS = true;
+    // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
+    // ENV.APP.LOG_VIEW_LOOKUPS = true;
+  }
+
+  if (environment === 'production') {
+    ENV.APP.API_HOST = (process.env.API_HOST || 'http://159.203.9.104:5000')
     // ENV.APP.LOG_RESOLVER = true;
     // ENV.APP.LOG_ACTIVE_GENERATION = true;
     // ENV.APP.LOG_TRANSITIONS = true;
